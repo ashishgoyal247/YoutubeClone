e670f93ec187edadbfed00d7da5acc3524d1d083
Ashish Goyal <ashishgoyal247@gmail.com>
2018-09-01 23:29:24 +0530

    Added search box

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
 //Create a new component that produce some component.
 
-const App = function() {
-  return <div>Hi</div>;
+const API_KEY = 'AIzaSyBp-kJhrq688ATlrD7sMR4SG5TvaqCA88E';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'Human'}, (data) => {
+      this.setState({videos: data});
+    });
+  } 
+
+  render() {
+    return (
+      <div>
+        <SearchBar /> 
+        <VideoList videos = {this.state.videos} />
+      </div>
+    );
+  }    
 }
 
 //Take this component geneated HTML and put it on the page(in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
